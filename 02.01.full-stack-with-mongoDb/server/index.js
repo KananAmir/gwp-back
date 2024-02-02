0147fbe142c89a9c243d0955197e3535cbe2b8e5
KananAmir <amirovknn@gmail.com>
2024-02-02 12:01:24 +0400

    fix & dotenv added

--- 02.01.full-stack-with-mongoDb/server/index.js
+++ 02.01.full-stack-with-mongoDb/server/index.js
@@ -4,16 +4,17 @@ const cors = require("cors");
 const mongoose = require("mongoose");
 const { Schema } = mongoose;
 
+require("dotenv").config();
 const app = express();
-const PORT = 8080;
-const DB_URL = `mongodb+srv://amirovknn:gwp123@cluster0.46hkp7d.mongodb.net/`;
+const PORT = process.env.PORT || 8000;
+const DB = process.env.DB_URL;
 
 app.use(cors());
 app.use(bodyParser.json({ limit: "50mb" }));
 app.use(bodyParser.urlencoded({ limit: "50mb", extended: true }));
 
 mongoose
-  .connect(DB_URL)
+  .connect(DB)
   .then(() => {
     console.log("connected to db succesfully!");
     app.listen(PORT, () => {
@@ -24,12 +25,15 @@ mongoose
     console.log(err);
   });
 
-const productScheam = new Schema({
-  title: { type: String, required: true },
-  price: { type: Number, requred: true },
-  description: { type: String, required: true },
-  image: { type: String, required: true },
-});
+const productScheam = new Schema(
+  {
+    title: { type: String, required: true },
+    price: { type: Number, required: true },
+    description: { type: String, required: true },
+    image: { type: String, required: true },
+  },
+  { timestamps: true }
+);
 
 const Products = mongoose.model("Products", productScheam);
 
@@ -60,10 +64,15 @@ app.get("/products/:id", async (req, res) => {
 app.delete("/products/:id", async (req, res) => {
   const { id } = req.params;
   try {
-    const deletedProducts = await Products.findByIdAndDelete(id);
+    const deletedProduct = await Products.findByIdAndDelete(id);
     const products = await Products.find({});
     // res.status(200).send(products);
-    res.status(200).send(deletedProducts);
+    // res.status(200).send(deletedProduct);
+    res.status(200).json({
+      message: "success",
+      deletedProduct: deletedProduct,
+      allProducts: products,
+    });
   } catch (error) {
     res.status(500).send({ message: error.message });
   }
@@ -72,8 +81,15 @@ app.delete("/products/:id", async (req, res) => {
 //post new product
 
 app.post("/products", async (req, res) => {
-  const newProduct = new Products(req.body);
+  // const { title, price, description, image } = req.body;
+  // const newProduct = new Products({
+  //   title,
+  //   price,
+  //   description,
+  //   image,
+  // });
 
+  const newProduct = new Products({ ...req.body });
   try {
     await newProduct.save();
     res.status(201).send({
@@ -90,7 +106,7 @@ app.post("/products", async (req, res) => {
 app.put("/products/:id", async (req, res) => {
   const { id } = req.params;
   try {
-    await Products.findByIdAndUpdate(id, req.body);
+    await Products.findByIdAndUpdate(id, { ...req.body });
     const updatedProduct = await Products.findById(id);
     res.status(200).send({
       message: "updated succesfully!",
